11d1ca6b4c70470a643155daec6e57b67188b6ad
Carlos Gámez <hardpx@gmail.com>
2024-08-17 14:16:46 -0600

    refactor

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -1,38 +1,32 @@
 import { useState } from 'react';
 
-const CounterApp = () => {
-  const [{ count1, count2, count3 }, setCount] = useState({
-    count1: 10,
-    count2: 20,
-    count3: 30,
-  });
+export const CounterApp = () => {
 
-  const handleClick = () =>
-    setCount((prevCount) => ({ ...prevCount, count1: count1 + 1 }));
+    const [ state, setCounter] = useState({
+        counter1: 10,
+        counter2: 20,
+        counter3: 30,
+    });
+
+    const { counter1, counter2, counter3 } = state;
 
   return (
-    <div className='w-1/4 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-auto mt-5'>
-      <h2 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
-        Counter: {count1}
-      </h2>
-      <hr />
-      <h2 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
-        Counter: {count2}
-      </h2>
-      <hr />
-      <h2 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
-        Counter: {count3}
-      </h2>
-      <hr />
-      <button
-        onClick={handleClick}
-        type='button'
-        className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold mt-4 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 w-full'
-      >
-        +1
-      </button>
-    </div>
-  );
-};
+    <>
+        <h1>Counter1: { counter1 }</h1>
+        <h1>Counter2: { counter2 }</h1>
+        <h1>Counter3: { counter3 }</h1>
 
-export default CounterApp;
+        <hr />
+
+        <button 
+            className="btn" 
+            onClick={ 
+                () => setCounter({
+                    ...state,
+                    counter1: counter1 + 1,
+                })
+            }>+1</button>
+    
+    </>
+  )
+}
